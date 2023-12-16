a464f0aa29c395fa06ce80d227b187101d4004c0
MatasKvn <matas.kvainauskas@protonmail.com>
2023-12-16 19:49:13 +0200

    Implemented simple HTTP server

--- server/Server.hs
+++ server/Server.hs
@@ -2,8 +2,32 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
 
 module Main where
 
+import Control.Concurrent
+import Control.Monad
+import DataFrame
+import Lib1 (renderDataFrameAsTable)
+import Web.Scotty
+import Data.Text.Lazy.Encoding (decodeUtf8)
+import ParsingParsec
+import Data.ByteString.Lazy.Char8 as LBS ( unpack, pack)
+import Data.Text.Lazy as TL (pack)
+
+
+
 main :: IO ()
-main = putStrLn "Ran SERVER"
+main = scotty 8080 $ do
+    post "/" $ do
+        body <- body
+        let idk = ParsingParsec.parseSql (LBS.unpack body)
+        case idk of
+            Left err -> text $ "An error occured.\n" <> TL.pack err
+            Right parsedStatement -> text $ "Statement parsed: " <> TL.pack (show parsedStatement)
+        
+-- run server & run
+-- curl -X POST -d "select * from a;" http://localhost:8080
+-- for calling this post method
